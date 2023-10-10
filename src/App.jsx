2a1d459c105c71ae44a8b78fa452b47585f21fff
MXCAZ <final00717@hotmail.com>
2023-10-09 20:16:40 -0600

    fixing issues with the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./components/routes";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
